be0b9c324465ceeef5e99d62a30907c332e27974
Steve Hu <stevehu@gmail.com>
2017-12-24 20:03:27 -0500

    add slf4j and logback

--- exec-core/build.gradle.kts
+++ exec-core/build.gradle.kts
@@ -3,6 +3,8 @@ plugins {
 }
 
 dependencies {
+    compile("org.slf4j:slf4j-api:1.7.25")
     testCompile("com.networknt:service:1.5.5")
     testCompile("junit:junit:4.12")
+    testCompile("ch.qos.logback:logback-classic:1.2.3")
 }
