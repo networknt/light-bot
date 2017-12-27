aec926bd3d443b5f2668e963718d2871acf4d903
Steve Hu <stevehu@gmail.com>
2017-12-26 22:38:17 -0500

    add default config for bot-cli

--- bot-cli/build.gradle.kts
+++ bot-cli/build.gradle.kts
@@ -10,4 +10,22 @@ dependencies {
     compile(project(":exec-core"))
     compile(project(":exec-develop"))
     compile("com.beust:jcommander:1.72")
+    compile("ch.qos.logback:logback-classic:1.2.3")
+}
+
+val fatJar = task("fatJar", type = Jar::class) {
+    baseName = "${project.name}-fat"
+    manifest {
+        attributes["Implementation-Title"] = "Gradle Jar File Example"
+        attributes["Implementation-Version"] = version
+        attributes["Main-Class"] = "com.networknt.bot.cli.Cli"
+    }
+    from(configurations.runtime.map({ if (it.isDirectory) it else zipTree(it) }))
+    with(tasks["jar"] as CopySpec)
+}
+
+tasks {
+    "build" {
+        dependsOn(fatJar)
+    }
 }
