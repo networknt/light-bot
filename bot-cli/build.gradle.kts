d8bc0f1e531bd7c20896e18b164f704b368428af
Steve Hu <stevehu@gmail.com>
2018-02-23 18:14:29 -0500

    fixes #18 upgrade light-4j framework 1.5.9

--- bot-cli/build.gradle.kts
+++ bot-cli/build.gradle.kts
@@ -13,7 +13,7 @@ dependencies {
     compile(project(":exec-release-maven"))
     compile(project(":exec-release-docker"))
     compile(project(":exec-regex-replace"))
-    compile("com.networknt:email:1.5.6")
+    compile("com.networknt:email:1.5.9")
     compile("com.beust:jcommander:1.72")
     compile("ch.qos.logback:logback-classic:1.2.3")
 }
