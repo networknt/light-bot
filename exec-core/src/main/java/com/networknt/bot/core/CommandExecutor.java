df83a37804cc090bf05490f494e084750b06d217
Steve Hu <stevehu@gmail.com>
2024-02-24 16:35:51 -0500

    fixes #125 add maven build for java 11

--- exec-core/src/main/java/com/networknt/bot/core/CommandExecutor.java
+++ exec-core/src/main/java/com/networknt/bot/core/CommandExecutor.java
@@ -12,32 +12,6 @@ import java.util.List;
 import java.util.Map;
 
 /**
- * This class can be used to execute a system command from a Java application.
- * See the documentation for the public methods of this class for more
- * information.
- *
- * Documentation for this class is available at this URL:
- *
- * http://devdaily.com/java/java-processbuilder-process-system-exec
- *
- *
- * Copyright 2010 alvin j. alexander, devdaily.com.
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
-
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser Public License for more details.
-
- * You should have received a copy of the GNU Lesser Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- * Please ee the following page for the LGPL license:
- * http://www.gnu.org/licenses/lgpl.txt
  * @deprecated
  */
 public abstract class CommandExecutor implements Executor {
@@ -48,23 +22,6 @@ public abstract class CommandExecutor implements Executor {
     private ThreadedStreamHandler errorStreamHandler;
     public static List<Process> processes = new ArrayList<>();
 
-    /**
-     * Pass in the system command you want to run as a List of Strings, as shown here:
-     *
-     * List<String> commands = new ArrayList<String>();
-     * commands.add("/sbin/ping");
-     * commands.add("-c");
-     * commands.add("5");
-     * commands.add("www.google.com");
-     * SystemCommandExecutor commandExecutor = new SystemCommandExecutor(commands);
-     * commandExecutor.executeCommand();
-     *
-     * Note: I've removed the other constructor that was here to support executing
-     *       the sudo command. I'll add that back in when I get the sudo command
-     *       working to the point where it won't hang when the given password is
-     *       wrong.
-     *
-     */
     public CommandExecutor() {
 
     }
--- exec-core/src/main/java/com/networknt/bot/core/ThreadedStreamHandler.java
+++ exec-core/src/main/java/com/networknt/bot/core/ThreadedStreamHandler.java
@@ -2,38 +2,6 @@ package com.networknt.bot.core;
 
 import java.io.*;
 
-/**
- * This class is intended to be used with the SystemCommandExecutor
- * class to let users execute system commands from Java applications.
- *
- * This class is based on work that was shared in a JavaWorld article
- * named "When System.exec() won't". That article is available at this
- * url:
- *
- * http://www.javaworld.com/javaworld/jw-12-2000/jw-1229-traps.html
- *
- * Documentation for this class is available at this URL:
- *
- * http://devdaily.com/java/java-processbuilder-process-system-exec
- *
- *
- * Copyright 2010 alvin j. alexander, devdaily.com.
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser Public License for more details.
- * You should have received a copy of the GNU Lesser Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- * Please ee the following page for the LGPL license:
- * http://www.gnu.org/licenses/lgpl.txt
- *
- */
 public class ThreadedStreamHandler extends Thread {
     InputStream inputStream;
     String adminPassword;
